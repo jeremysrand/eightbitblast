941a5f85f3f6bec138598474e0a4b5c85a30ce94
Jeremy Rand <jeremy@rand-family.com>
2023-07-21 20:26:18 -0500

    Initial Commit

---
+++ eightbitblast/main.c
@@ -0,0 +1,20 @@
+/*
+ * main.c
+ * eightbitblast
+ *
+ * Created by Jeremy Rand on 2023-07-21.
+ * Copyright (c) 2023 ___ORGANIZATIONNAME___. All rights reserved.
+ *
+ */
+
+
+#include <stdio.h>
+#include <conio.h>
+
+
+int main(void)
+{
+    printf("HELLO, WORLD!\n");
+    cgetc();
+    return 0;
+}
